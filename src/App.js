2672340fa08b2aa6e83f0b022a224e99789f814d
Monica Delgado <mnca.delgado@gmail.com>
2022-03-21 22:49:53 -1100

    added an unordered list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Setup the IDE'
+  },
+  {
+    id: 2,
+    title: 'Create a new folder'
+  },
+  {
+    id: 3,
+    title:'Initiate the new React App'
+  },
+  {
+    id: 4,
+    title: 'Start working'
+  },
+  {
+    id: 5,
+    title: 'Have fun!'
+  }
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+        return (
+          <li key={item.id}>
+            {item.title}
+            </li>
+        )
+        })}
+      </ul>
     </div>
   );
 }
